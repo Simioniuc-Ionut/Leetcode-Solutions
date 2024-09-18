2ce585043878d0639cab4210fcbb10b7751217cd
Simioniuc Ionut-Mihai <119358047+Simioniuc-Ionut@users.noreply.github.com>
2024-09-18 15:43:40 +0000

    up

---
+++ Jewels and Stone - Leetcode 771/Jewels and Stone - Leetcode 771.java
@@ -0,0 +1,16 @@
+
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        // Creăm un set de caractere unice pentru "jewels"
+        Set<Character> unique_jewels = new HashSet<>();
+        unique_jewels = jewels.chars().mapToObj(c -> (char) c).collect(Collectors.toSet());
+        int c = 0;
+        for(int i=0;i < stones.length(); i++){
+            char w = stones.charAt(i);
+            if (unique_jewels.contains(w)){
+                c++;
+            }
+        }
+        return c;
+    }
+}
